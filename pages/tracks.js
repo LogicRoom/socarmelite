4653916d8c61dbd55ba34f5654130d04028bc2e5
peter_heard01 <pete@logicroom.co>
2025-02-04 16:27:13 +0000

    updated all tracks

--- pages/tracks.js
+++ pages/tracks.js
@@ -24,6 +24,38 @@ const tracks = [
     beatportLink: "",
   },
   {
+    title: "Never Meant To (4 of 52)",
+    soundcloudEmbed:
+      "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1996280119%3Fsecret_token%3Ds-9bTQKyqmocz&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    dropboxLink: "https://www.dropbox.com/scl/fi/7ptxp9a86x1p5rf26u2iz/Carmelite-Never-Meant-To.mp3?rlkey=4tvyud2t7yk2gc5fp156b253h&dl=0",
+    status: "released",
+    beatportLink: "",
+  },
+  {
+    title: "Hit Me Up (5 of 52)",
+    soundcloudEmbed:
+      "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2008248903%3Fsecret_token%3Ds-9LixMJFP8qY&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    dropboxLink: "https://www.dropbox.com/scl/fi/yqvxd7wpij2bb893or0yu/Carmelite-Hit-Me-Up.mp3?rlkey=kah17elt3wiwbjgufj7q746by&dl=0",
+    status: "released",
+    beatportLink: "",
+  },
+  {
+    title: "Nothing To Fear (6 of 52)",
+    soundcloudEmbed:
+      "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2000345611%3Fsecret_token%3Ds-r1VizeB0Hu5&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    dropboxLink: "https://www.dropbox.com/scl/fi/4ou9wpvkx3fxuf8pxtm5u/Carmelite-Nothing-To-Fear.mp3?rlkey=r5qaah2f37c88hwgkys0pf9jz&dl=0",
+    status: "released",
+    beatportLink: "",
+  },
+  {
+    title: "Tearing Through My Heart (7 of 52)",
+    soundcloudEmbed:
+      "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2008250239%3Fsecret_token%3Ds-uJjvKLZmcxg&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    dropboxLink: "https://www.dropbox.com/scl/fi/s07hjy5offlwatxdr5o9v/Carmelite-Tearing-Through-My-Heart.mp3?rlkey=pudfgwszkauoj0ihvu4cfqips&dl=0",
+    status: "review",
+    beatportLink: "",
+  },
+  {
     title: "I Just Want You (8 of 52)",
     soundcloudEmbed:
       "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2010567227%3Fsecret_token%3Ds-nlvtlqQUVrf&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
