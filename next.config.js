c033a78b0e6ef5ef0c31013aa214d940fd66a04f
peter_heard01 <pete@logicroom.co>
2026-02-26 15:16:05 +0000

    next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/sessions.html', // Ensure this matches your exact filename in the public folder
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
